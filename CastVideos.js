7dec04d27cc87d4ec74777bfc762a287c742bb3a
Mattias Schlenker <ms@mattiasschlenker.de>
2014-08-29 12:49:02 +0200

    revert

--- CastVideos.js
+++ CastVideos.js
@@ -368,7 +368,7 @@ CastPlayer.prototype.selectMedia = function(mediaIndex) {
  */
 CastPlayer.prototype.launchApp = function() {
   console.log("launching app...");
-  this.togglePlayer.bind(this); 
+  // this.togglePlayer.bind(this); 
   chrome.cast.requestSession(
     this.sessionListener.bind(this),
     this.onLaunchError.bind(this));
@@ -402,7 +402,7 @@ CastPlayer.prototype.onLaunchError = function() {
  * Stops the running receiver application associated with the session.
  */
 CastPlayer.prototype.stopApp = function() {
-  this.togglePlayer.bind(this); 
+  // this.togglePlayer.bind(this); 
   this.session.stop(this.onStopAppSuccess.bind(this, 'Session stopped'),
       this.onError.bind(this));    
 };
